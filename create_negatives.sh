2099900cafcf85fd87e48db7deebaa4354cfbb6a
Simon Hofmann <mail@simon-hofmann.org>
2014-08-21 01:17:06 +0200

    Separated file list creation from image creation.

--- create_negatives.sh
+++ create_negatives.sh
@@ -2,8 +2,6 @@
 
 # Create random samples used as negative images for opencv_traincascade
 # Args:
-# $1: Image directory
-# $2: Output directory
 
 function create_negatives () {
     for entry in $1/*
@@ -15,15 +13,17 @@ function create_negatives () {
             if [[ `file -ib $entry | grep 'image'` ]]
             then
                 count=0
-                # Statically generate three samples.
+                # Statically generate five samples.
                 # TODO: Dynamic version is not working due to variable scope in loops. Fix this...
-                while [ $count -lt 3 ]
+                while [ $count -lt 5 ]
                 do
-                    x=$((RANDOM%1000+1000))
-                    y=$((RANDOM%1000+1000))
-
+                    # CONFIGURE IMAGE SIZE HERE
+                    x=$((RANDOM%1000+500))
+                    y=$((RANDOM%1000+500))
+                    
+                    # CONFIGURE IMAGE OFFSET HERE
                     off_x=$((RANDOM%500))
-                    off_y=$((RANDOM%300))
+                    off_y=$((RANDOM%500))
 
                     echo "Processing $(basename $entry) ..."
 
@@ -41,22 +41,16 @@ function create_negatives () {
 }
 
 # Parameter check, argument 1 and 2 should be directories.
-if [[ -d $1 ]] || [[ -d $2 ]]
+if [[ -d $1 ]] && [[ -d $2 ]]
 then
-    if [ `mkdir -p $2` ]
-    then
-
-        create_negatives $1 $2
-
-        #Create file list for use with opencv_traincascade
-        find $2 -type f -not -iname "*.txt" >> $2/neg.txt
-    else
-        echo "Unable to create output directory"
-        exit 1
-    fi
+    create_negatives $1 $2
+elif [[ $1 == "-list" ]] && [[ -d $2 ]]
+then
+    #Create file list for use with opencv_traincascade
+    find $2 -type f -not -iname "*.txt" >> $2/neg.txt
 else
     echo "Usage: create_negatives arg1 arg2"
     echo ""
-    echo "arg1: Directory containing the input images."
+    echo "arg1: Directory containing the input images or '-list'."
     echo "arg2: Directory to store output images."
 fi
